357eb6bfe8a1d76ee1904626f926609c9fa7cf5a
Muhammad Iqbal CR <muh.iqbal.cr@gmail.com>
2023-12-15 21:51:36 -0600

    Added JSON output in firebase_list_devices.py.

--- firebase_list_devices.py
+++ firebase_list_devices.py
@@ -1,8 +1,10 @@
 import argparse
+import csv
 from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import json
 import logging
 import sys
 
@@ -33,22 +35,34 @@ def list_devices(args):
         [val["mac"].replace(":", "-") for val in heartbeats]))
     logging.debug(macs)
 
-    outstring = "mac,online,last_timestamp\n"
+    outarr = list()
     now_timestamp = datetime.timestamp(datetime.now()) * 1000
 
     for mac in macs:
         last_timestamp = get_last_timestamp(heartbeats, mac)
-        outstring += "{},{},{}\n".format(
-            mac,
-            (now_timestamp - last_timestamp) < 3600000,  # 1 hour
-            datetime.fromtimestamp(
-                last_timestamp / 1000).astimezone().isoformat())
+        outarr.append({
+            "mac": mac,
+            "online": (now_timestamp - last_timestamp) < 3600000,  # 1 hour
+            "last_timestamp": datetime.fromtimestamp(
+                last_timestamp / 1000).astimezone().isoformat()
+        })
 
-    args.output_file.write(outstring)
+    outarr = sorted(outarr, key=lambda x: (~x["online"], x["mac"]))
+    logging.debug(outarr)
+
+    if (args.json):
+        args.output_file.write(json.dumps(outarr))
+    else:
+        csv_writer = csv.DictWriter(
+            args.output_file, fieldnames=["mac", "online", "last_timestamp"])
+        csv_writer.writeheader()
+        csv_writer.writerows(outarr)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument("-J", "--json", action="store_true",
+                        help="Output as JSON.")
     parser.add_argument("-o", "--output-file", nargs='?',
                         type=argparse.FileType('w'), default=sys.stdout,
                         help="Output result to file, default is to stdout'")
