7bcfeac77b2ac4819b95a2729b411bc88a132e1a
Muhammad Iqbal CR <muh.iqbal.cr@gmail.com>
2024-05-22 17:36:14 -0500

    Removing the copying of program log, added parallel processing.

--- batch_extract.py
+++ batch_extract.py
@@ -3,9 +3,9 @@ import argparse
 from datetime import datetime
 import json
 import logging
+from multiprocessing import Pool
 from pathlib import Path
 import subprocess
-from shutil import copy
 import firebase_admin
 from firebase_admin import credentials
 import firebase_download
@@ -13,10 +13,151 @@ import firebase_list_devices
 import write_csv
 
 
-def batch_extract(args):
+args = None
+rpi_ids = None
+heartbeats = None
+
+
+def process_mac(mac):
+    # 3.1. Write tput
+    print(f"Writing throughput CSV and JSON for {mac}...")
+    out_tput = write_csv.read_logs(write_csv.parse(
+        ["throughput",
+         "-d", str(args.log_dir),
+         "-m", mac,
+         "-l", args.log_level]))
+    if (len(out_tput) > 0):
+        write_csv.write(out_tput, write_csv.parse(
+            ["throughput", "-J",
+             "-o", str(args.outdir.joinpath(
+                       "throughput_{}.json".format(mac))),
+             "-l", args.log_level]))
+        write_csv.write(out_tput, write_csv.parse(
+            ["throughput",
+             "-o", str(args.outdir.joinpath(
+                       "throughput_{}.csv".format(mac))),
+             "-l", args.log_level]))
+    else:
+        logging.warning(f"No throughput logs for {mac}! Skipping...")
+
+    # 3.2. Write lat
+    print(f"Writing latency CSV and JSON for {mac}...")
+    out_lat = write_csv.read_logs(write_csv.parse(
+        ["latency",
+         "-d", str(args.log_dir),
+         "-m", mac,
+         "-l", args.log_level]))
+    if (len(out_lat) > 0):
+        write_csv.write(out_lat, write_csv.parse(
+            ["latency", "-J",
+             "-o", str(args.outdir.joinpath(
+                       "latency_{}.json".format(mac))),
+             "-l", args.log_level]))
+        write_csv.write(out_lat, write_csv.parse(
+            ["latency",
+             "-o", str(args.outdir.joinpath(
+                       "latency_{}.csv".format(mac))),
+             "-l", args.log_level]))
+    else:
+        logging.warning(f"No latency logs for {mac}! Skipping...")
+
+    # 3.3. Write wifi_scan
+    print(f"Writing wifi scan CSV and JSON for {mac}...")
+    out_scan = write_csv.read_logs(write_csv.parse(
+        ["wifi_scan",
+         "-d", str(args.log_dir),
+         "-m", mac,
+         "-l", args.log_level]))
+    if (len(out_scan) > 0):
+        write_csv.write(out_scan, write_csv.parse(
+            ["wifi_scan", "-J",
+             "-o", str(args.outdir.joinpath(
+                       "wifi_scan_{}.json".format(mac))),
+             "-l", args.log_level]))
+        write_csv.write(out_scan, write_csv.parse(
+            ["wifi_scan",
+             "-o", str(args.outdir.joinpath(
+                       "wifi_scan_{}.csv".format(mac))),
+             "-l", args.log_level]))
+    else:
+        logging.warning(f"No Wi-Fi scan logs for {mac}! Skipping...")
+
+    # 3.4 Write aggregate throughput tests
+    print(f"Writing aggregate tput CSV and JSON for {mac}...")
+    if len(out_tput) > 0:
+        agg_tput_df = agg_tput.create_csv(out_tput, out_scan)
+        if (agg_tput_df is not None):
+            agg_tput.write(agg_tput_df, agg_tput.parse(
+                [mac,
+                 "-J", "-o", str(args.outdir.joinpath(
+                                 f"agg_tput_{mac}.json")),
+                 "-l", args.log_level]))
+            agg_tput.write(agg_tput_df, agg_tput.parse(
+                [mac,
+                 "-o", str(args.outdir.joinpath(f"agg_tput_{mac}.json")),
+                 "-l", args.log_level]))
+        else:
+            logging.warning(f"No aggregate tput for {mac}! Skipping...")
+    else:
+        logging.warning(f"No throughput logs for {mac}! Skipping...")
+
+    # 3.5. Write heartbeats
+    out_hbeat = firebase_list_devices.get_mac(heartbeats, mac, rpi_ids)
+    print(f"Writing heartbeat CSV and JSON for {mac}...")
+    if (len(out_hbeat) > 0):
+        firebase_list_devices.write(
+            out_hbeat,
+            firebase_list_devices.parse(
+                ["-J", "-m", mac, "-o", str(args.outdir.joinpath(
+                    "heartbeat_{}.json".format(mac))),
+                 "-l", args.log_level]))
+        firebase_list_devices.write(
+            out_hbeat,
+            firebase_list_devices.parse(
+                ["-m", mac, "-o", str(args.outdir.joinpath(
+                    "heartbeat_{}.csv".format(mac))),
+                 "-l", args.log_level]))
+    else:
+        logging.warning(f"No HB logs for {mac}! Skipping...")
+
+    # 3.6. Zip CSVs & JSONs
+    print(f"Compressing CSV and JSON files for {mac}...")
+    if (len(out_tput) > 0
+            or len(out_lat) > 0
+            or len(out_scan) > 0
+            or len(out_hbeat) > 0):
+        cmd_out = subprocess.run(
+            ["zip", "-ur",
+             str(args.outdir.joinpath(
+                 "data_{}.zip".format(mac)).resolve()),
+             ".", "-i",
+             "*_{}.json".format(mac),
+             "*_{}.csv".format(mac)],
+            cwd=str(args.outdir.resolve()),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT).stdout.decode("utf-8")
+        logging.info(cmd_out)
+    else:
+        logging.warning(f"No CSV or JSON files for {mac}! Skipping...")
+
+    # 3.7. Zip raw logs
+    print(f"Compressing raw logs for {mac}...")
+    if (args.log_dir.joinpath(mac).is_dir()):
+        cmd_out = subprocess.run(
+            ["zip", "-ur",
+             str(args.outdir.joinpath(
+                 "rawlogs_{}.zip".format(mac)).resolve()),
+             "."],
+            cwd=str(args.log_dir.joinpath(mac).resolve()),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT).stdout.decode("utf-8")
+        logging.info(cmd_out)
+
+
+def batch_extract():
     # Setup
+    global rpi_ids, heartbeats
     logging.basicConfig(level=args.log_level.upper())
-    rpi_ids = None
     rpi_config = Path(".rpi-config.json")
     if rpi_config.is_file():
         with open(rpi_config) as file:
@@ -54,148 +195,11 @@ def batch_extract(args):
              "-l", args.log_level]))
 
     print("3. Write CSV and JSON for each MAC")
-    for mac in macs:
-        # 3.1. Write tput
-        print(f"Writing throughput CSV and JSON for {mac}...")
-        out_tput = write_csv.read_logs(write_csv.parse(
-            ["throughput",
-             "-d", str(args.log_dir),
-             "-m", mac,
-             "-l", args.log_level]))
-        if (len(out_tput) > 0):
-            write_csv.write(out_tput, write_csv.parse(
-                ["throughput", "-J",
-                 "-o", str(args.outdir.joinpath(
-                           "throughput_{}.json".format(mac))),
-                 "-l", args.log_level]))
-            write_csv.write(out_tput, write_csv.parse(
-                ["throughput",
-                 "-o", str(args.outdir.joinpath(
-                           "throughput_{}.csv".format(mac))),
-                 "-l", args.log_level]))
-        else:
-            logging.warning(f"No throughput logs for {mac}! Skipping...")
-
-        # 3.2. Write lat
-        print(f"Writing latency CSV and JSON for {mac}...")
-        out_lat = write_csv.read_logs(write_csv.parse(
-            ["latency",
-             "-d", str(args.log_dir),
-             "-m", mac,
-             "-l", args.log_level]))
-        if (len(out_lat) > 0):
-            write_csv.write(out_lat, write_csv.parse(
-                ["latency", "-J",
-                 "-o", str(args.outdir.joinpath(
-                           "latency_{}.json".format(mac))),
-                 "-l", args.log_level]))
-            write_csv.write(out_lat, write_csv.parse(
-                ["latency",
-                 "-o", str(args.outdir.joinpath(
-                           "latency_{}.csv".format(mac))),
-                 "-l", args.log_level]))
-        else:
-            logging.warning(f"No latency logs for {mac}! Skipping...")
-
-        # 3.3. Write wifi_scan
-        print(f"Writing wifi scan CSV and JSON for {mac}...")
-        out_scan = write_csv.read_logs(write_csv.parse(
-            ["wifi_scan",
-             "-d", str(args.log_dir),
-             "-m", mac,
-             "-l", args.log_level]))
-        if (len(out_scan) > 0):
-            write_csv.write(out_scan, write_csv.parse(
-                ["wifi_scan", "-J",
-                 "-o", str(args.outdir.joinpath(
-                           "wifi_scan_{}.json".format(mac))),
-                 "-l", args.log_level]))
-            write_csv.write(out_scan, write_csv.parse(
-                ["wifi_scan",
-                 "-o", str(args.outdir.joinpath(
-                           "wifi_scan_{}.csv".format(mac))),
-                 "-l", args.log_level]))
-        else:
-            logging.warning(f"No Wi-Fi scan logs for {mac}! Skipping...")
-
-        # 3.4 Write aggregate throughput tests
-        print(f"Writing aggregate tput CSV and JSON for {mac}...")
-        if len(out_tput) > 0:
-            agg_tput_df = agg_tput.create_csv(out_tput, out_scan)
-            if (agg_tput_df is not None):
-                agg_tput.write(agg_tput_df, agg_tput.parse(
-                    [mac,
-                     "-J", "-o", str(args.outdir.joinpath(
-                                     f"agg_tput_{mac}.json")),
-                     "-l", args.log_level]))
-                agg_tput.write(agg_tput_df, agg_tput.parse(
-                    [mac,
-                     "-o", str(args.outdir.joinpath(f"agg_tput_{mac}.json")),
-                     "-l", args.log_level]))
-            else:
-                logging.warning(f"No aggregate tput for {mac}! Skipping...")
-        else:
-            logging.warning(f"No throughput logs for {mac}! Skipping...")
-
-        # 3.5. Write heartbeats
-        out_hbeat = firebase_list_devices.get_mac(heartbeats, mac, rpi_ids)
-        print(f"Writing heartbeat CSV and JSON for {mac}...")
-        if (len(out_hbeat) > 0):
-            firebase_list_devices.write(
-                out_hbeat,
-                firebase_list_devices.parse(
-                    ["-J", "-m", mac, "-o", str(args.outdir.joinpath(
-                        "heartbeat_{}.json".format(mac))),
-                     "-l", args.log_level]))
-            firebase_list_devices.write(
-                out_hbeat,
-                firebase_list_devices.parse(
-                    ["-m", mac, "-o", str(args.outdir.joinpath(
-                        "heartbeat_{}.csv".format(mac))),
-                     "-l", args.log_level]))
-        else:
-            logging.warning(f"No HB logs for {mac}! Skipping...")
-
-        # 3.6. Zip CSVs & JSONs
-        print(f"Compressing CSV and JSON files for {mac}...")
-        if (len(out_tput) > 0
-                or len(out_lat) > 0
-                or len(out_scan) > 0
-                or len(out_hbeat) > 0):
-            cmd_out = subprocess.run(
-                ["zip", "-ur",
-                 str(args.outdir.joinpath(
-                     "data_{}.zip".format(mac)).resolve()),
-                 ".", "-i",
-                 "*_{}.json".format(mac),
-                 "*_{}.csv".format(mac)],
-                cwd=str(args.outdir.resolve()),
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT).stdout.decode("utf-8")
-            logging.info(cmd_out)
-        else:
-            logging.warning(f"No CSV or JSON files for {mac}! Skipping...")
-
-        # 3.7. Zip raw logs
-        print(f"Compressing raw logs for {mac}...")
-        if (args.log_dir.joinpath(mac).is_dir()):
-            cmd_out = subprocess.run(
-                ["zip", "-ur",
-                 str(args.outdir.joinpath(
-                     "rawlogs_{}.zip".format(mac)).resolve()),
-                 "."],
-                cwd=str(args.log_dir.joinpath(mac).resolve()),
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT).stdout.decode("utf-8")
-            logging.info(cmd_out)
-
-        # 3.8. Copy main program log
-        # print(f"Copying main program log for {mac}...")
-        # try:
-        #     copy(args.log_dir / mac / "speedtest_logger.log",
-        #          args.outdir / f"speedtest_logger_{mac}.log")
-        # except Exception as e:
-        #     logging.warning("Copy error: %s", e)
+    if args.num_parallel > 1:
+        print(f"Running {args.num_parallel} parallel processes, log texts "
+              "may be unordered.")
+    with Pool(args.num_parallel) as p:
+        p.map(process_mac, macs)
 
     print("4. Compress all CSVs & JSONs")
     cmd_out = subprocess.run(
@@ -222,6 +226,8 @@ def parse(list_args=None):
                         help="Specify output directory.")
     parser.add_argument("-d", "--log-dir", type=Path, default=Path("./logs"),
                         help="Specify local log directory, default='./logs'")
+    parser.add_argument("-p", "--num-parallel", default=8,
+                        help="Specify local log directory, default='./logs'")
     parser.add_argument("--rsync", action="store_true",
                         help="Use rsync to server instead of Firebase")
     parser.add_argument("-l", "--log-level", default="warning",
@@ -239,4 +245,5 @@ if __name__ == '__main__':
         "storageBucket": "nd-schmidt.appspot.com",
         "databaseURL": "https://nd-schmidt-default-rtdb.firebaseio.com"
     })
-    batch_extract(parse())
+    args = parse()
+    batch_extract()
